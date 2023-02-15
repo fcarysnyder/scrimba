c80c8ee2e4ee770bbe624c813980d7eda1985112
cary.snyder <cary.snyder@umbra.space>
2023-02-15 12:13:24 -0800

    Co-authored-by: Matt Williams <mpwilliams89@gmail.com>
    Refactored to remove redundant functions

--- index.js
+++ index.js
@@ -1,43 +1,16 @@
 let scoreHome = document.getElementById("score-home")
 let scoreAway = document.getElementById("score-away")
 
-let score1 = 0
-let score2 = 0
+let x = 0
+let y = 0
 
-function plusOneHome() {
-    score1 += 1
-    scoreHome.innerText = score1
-}
-
-function plusTwoHome() {
-    score1 += 2
-    scoreHome.innerText = score1   
-}
-
-function plusThreeHome() {
-    score1 += 3
-    scoreHome.innerText = score1   
-}
-
-function plusOneAway() {
-    score2 += 1
-    scoreAway.innerText = score2   
-}
-
-function plusTwoAway() {
-    score2 += 2
-    scoreAway.innerText = score2   
-}
-
-function plusThreeAway() {
-    score2 += 3
-    scoreAway.innerText = score2   
+function calcScore (addToScore, score, scoreboardElement) {
+    scoreboardElement.textContent = score  
 }
 
 function resetScore() {
-    score1 = 0
-    score2 = 0
-    scoreHome.textContent = score1
-    scoreAway.textContent = score2
-    
+    x = 0
+    y = 0
+    scoreHome.textContent = x
+    scoreAway.textContent = y
 }
