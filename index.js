a02113c7be9d913b2ea82f680045d1de494f411b
cary.snyder <cary.snyder@umbra.space>
2023-02-17 11:13:27 -0800

    Updated with new function from Matt

--- index.js
+++ index.js
@@ -4,9 +4,15 @@ let scoreAway = document.getElementById("score-away")
 let x = 0
 let y = 0
 
-function calcScore (addToScore, score, scoreboardElement) {
-    scoreboardElement.textContent = score  
-}
+function addToTeamScore(value, team) {
+    if (team === "home") {
+      x += value
+      scoreHome.textContent = x
+    } else {
+      y += value
+      scoreAway.textContent = y
+    }
+  }
 
 function resetScore() {
     x = 0
